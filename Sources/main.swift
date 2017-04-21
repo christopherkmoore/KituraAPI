3e2cc352d7c654c3bb8055550a0df36dd1a07f0f
christopherkmoore <christopherkmoore1@gmail.com>
2017-04-21 13:42:03 -0700

    add: router

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,15 @@
-print("Hello, world!")
+// Simple API for creating a "hello world" JSON response.
+
+import Kitura
+
+let router = Router()
+
+router.post("/") {
+	request, response, next in
+	response.send("Hello World")
+	next()
+}
+
+Kitura.addHTTPServer(onPort: 8090, with: router)
+
+Kitura.run()
