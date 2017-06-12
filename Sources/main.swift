0f1bf4630c86436eab5448fe3d733a4f45d0d6d9
Christopher Moore <christopher.moore@slalom.com>
2017-06-12 15:32:15 -0700

    add: Todo struct

--- Sources/main.swift
+++ Sources/main.swift
@@ -5,9 +5,36 @@ import Kitura
 
 let router = Router()
 
+struct Todo: Codable {
+    var title: String
+    var id: Int?
+    var userId: Int
+    var completed: Int
+}
+
+var todos = [Todo]()
+todos.append(Todo(title: "Hello World", id: 8, userId: 420, completed: 1))
+
+func encodeJSON(completion: @escaping (Data?, String?) -> Void) {
+    
+    let encoder = JSONEncoder()
+    do {
+        let data = try encoder.encode(todos)
+        completion(data, nil)
+    } catch {
+        print("we fucked up over here.") ; return
+    }
+}
+
 router.get("/HelloWorld") {
 	request, response, next in
-	response.send("Hello Kitura World")
+    encodeJSON() { (data, error) in
+        if let data = data {
+            response.send(data: data)
+        } else {
+            response.send("error")
+        }
+    }
 	next()
 }
 
